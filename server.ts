dc6fe123bd3be28c90c4b309b012d4de72bae626
boky0102 <bivankovi@protonmail.com>
2023-07-30 13:21:37 +0200

    Connected server to local mongoDB with error handling

--- server.ts
+++ server.ts
@@ -1,5 +1,25 @@
 import express, {Express, Request, Response} from 'express';
+import 'dotenv/config';
+import bodyParser from 'body-parser';
+import mongoose  from 'mongoose';
+
+
+//CONNECTING TO DB
+async function connectDB(){
+
+    if(process.env.DATABASE_URL){
+        await mongoose.connect(process.env.DATABASE_URL);
+        console.log("DATABASE CONNECTED SUCCESSFULLY");
+    } else{
+        throw new Error("env variable DATABASE_URL is undefined, please reconfigure your .env file");
+    }
+}
+
+connectDB().catch(e => console.log(e.message));
+
+
 const port = 3000
+
 
 const app: Express = express();
 
