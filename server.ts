ab1e4829862daf6bff2e4f3ce6e04564a80e4982
boky0102 <bivankovi@protonmail.com>
2023-08-02 19:52:21 +0200

    Switched db ORM to PRISMA because of bad typescript support in mongoose

--- server.ts
+++ server.ts
@@ -1,29 +1,11 @@
 import express, {Express, Request, Response} from 'express';
 import 'dotenv/config';
 import bodyParser from 'body-parser';
-import mongoose, { Error }  from 'mongoose';
-import UserModel from './modules/userModel';
-
+import { PrismaClient } from '@prisma/client';
 
 //CONNECTING TO DB
-async function connectDB(){
-    try{
-        if(process.env.DATABASE_URL){
-            await mongoose.connect(process.env.DATABASE_URL);
-            console.log("DATABASE CONNECTED SUCCESSFULLY");
-        } else{
-            throw new Error("env variable DATABASE_URL is undefined, please reconfigure your .env file");
-        }
-    } catch(error){
-        console.log(error)
-    }
-}
 
-connectDB();
-
-
-/* connectDB().catch(e => console.log(e.message));
- */
+const prisma = new PrismaClient();
 
 const port = 3000
 
@@ -31,8 +13,9 @@ const port = 3000
 const app: Express = express();
 
 
-app.get("/", (req: Request,res: Response) => {
-    res.send("Test is working")
+app.get("/", async (req: Request,res: Response) => {
+    const allUsers = await prisma.user.findMany();
+    res.send(allUsers);
 })
 
 
