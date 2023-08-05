c088edbe40aefa1a01c6f9ee3317dbe4e6c3823d
boky0102 <bivankovi@protonmail.com>
2023-08-05 15:08:03 +0200

    Added /register route

--- server.ts
+++ server.ts
@@ -2,7 +2,11 @@ import express, {Express, Request, Response} from 'express';
 import 'dotenv/config';
 import bodyParser from 'body-parser';
 import { PrismaClient } from '@prisma/client';
+import cors from 'cors';
 
+var corsOptions: cors.CorsOptions = {
+    origin: 'http://localhost:5173'
+}
 //CONNECTING TO DB
 
 const prisma = new PrismaClient();
@@ -12,10 +16,24 @@ const port = 3000
 
 const app: Express = express();
 
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get("/", async (req: Request,res: Response) => {
     const allUsers = await prisma.user.findMany();
     res.send(allUsers);
+})
+
+app.post("/register", async (req: Request, res: Response) => {
+    try{
+        const userData = req.body;
+        console.log(req.body);
+        res.status(200).send();
+
+    } catch(err){
+        console.log(err);
+    }
 })
 
 
